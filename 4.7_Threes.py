8792cfa50caa789373ad279aac9ce38bf0122c38
Veronica <veranika.aizu@gmail.com>
2020-03-16 16:33:37 +0900

    Update 4.7_Threes.py

--- 4.7_Threes.py
+++ 4.7_Threes.py
@@ -1,9 +1,16 @@
-#Make a list of the multiples of 3 from 3 to 30. Use a for loop to print the numbers in your list.
+# Make a list of the multiples of 3 from 3 to 30. Use a for loop to print the numbers in your list.
 
-multiply = []
-for value in list(range(3,31)):
-	number = value * 3
-	multiply.append(number)
-	print(number)
-print(multiply)
+multiples_of_three = []
+for value in range(3,31,3):
+    multiples_of_three.append(value)
 
+# #the same using list comprehensions
+multiples_of_three = [value for value in range(3,31,3)]
+
+# the same but using list()
+multiples_of_three = list(range(3,31,3))
+
+
+# finally printing numbers
+for number in multiples_of_three:
+    print(number)

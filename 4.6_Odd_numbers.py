520fe71263af475acbaffaaf35bb9c50b0b193a5
Veronica <veranika.aizu@gmail.com>
2020-03-16 14:52:34 +0900

    Chapter 4 ex.6

---
+++ 4.6_Odd_numbers.py
@@ -0,0 +1,4 @@
+#Use the third argument of the range() function to make a list of the odd numbers from 1 to 20. 
+#Use a for loop to print each number.
+even_numbers = list(range(2,21,2))
+print(even_numbers)

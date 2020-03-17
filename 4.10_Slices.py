b4f140314ae5931019ab103655bacd1edde7cb3a
Veronica <veranika.aizu@gmail.com>
2020-03-17 14:38:23 +0900

    Chapter 4 ex. 10

---
+++ 4.10_Slices.py
@@ -0,0 +1,11 @@
+# Print the message The first three items in the list are:. 
+# Then use a slice to print the first three items from that program’s list.
+languages = ['russian', 'belarusian', 'english', 'japanese','french']
+print("The first three items in the list are: ")
+print(languages[:3])
+# Three items from the middle of the list are:
+print("Three items from the middle of the list are: ")
+print(languages[1:4])
+# The last three items in the list are:
+print("The last three items in the list are: ")
+print(languages[-3:])

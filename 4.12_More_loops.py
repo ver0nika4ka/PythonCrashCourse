9702389b326ba50792fb8354967f11afd01d4d29
Veronica <veranika.aizu@gmail.com>
2020-03-18 16:01:21 +0900

    Code Rewiew PEP8

--- 4.12_More_loops.py
+++ 4.12_More_loops.py
@@ -1,4 +1,4 @@
-# write two for loops to print each list of foods.
+# Write two for loops to print each list of foods.
 my_foods = ['pizza', 'falafel', 'carrot cake'] 
 friend_foods = my_foods[:]
 
--- 4.13_Buffet.py
+++ 4.13_Buffet.py
@@ -1,7 +1,5 @@
-# A buffet-style restaurant offers only five basic foods. 
 # Think of five simple foods, and store them in a tuple.
 # Use a for loop to print each food the restaurant offers.
-
 buffet_food = ('noodles', 'potato', 'vegetables', 'meat', 'fish')
 print('Original menu: ')
 for food in buffet_food:
@@ -9,7 +7,8 @@ for food in buffet_food:
 # Try to modify one of the items, and make sure that Python rejects the change.
 # buffet_food.append = 'shrimp'
 # The restaurant changes its menu, replacing two of the items with different foods. 
-# Add a line that rewrites the tuple, and then use a for loop to print each of the items on the revised menu.
+# Add a line that rewrites the tuple
+# Use a for loop to print each of the items on the revised menu.
 modified_food = ('noodles', 'potato', 'vegetables', 'bread', 'rice')
 print('Modified menu: ')
 for m_food in modified_food:

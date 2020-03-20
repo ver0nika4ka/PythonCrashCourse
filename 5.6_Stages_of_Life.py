8f0d2cd87410ed6d58d6e01d28e9c5e3e3e29750
Veronica <veranika.aizu@gmail.com>
2020-03-20 18:27:55 +0900

    Chapter 5 ex. 6

---
+++ 5.6_Stages_of_Life.py
@@ -0,0 +1,14 @@
+person = 24
+
+if person < 2:
+    print("You are a baby!")
+elif (person >= 2) and (person < 4):
+    print("You are a toddler!")
+elif (person >= 4) and (person < 13):
+    print("You are a kid!")
+elif (person >= 13) and (person < 20):
+    print("You are a teenager!")
+elif (person >= 20) and (person < 65):
+    print("You are an adult!")
+else:
+    print("You are an elder!")

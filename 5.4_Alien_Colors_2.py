8965d6e5f139ac029f382cc2a89cf78da5cec8dd
Veronica <veranika.aizu@gmail.com>
2020-03-20 18:10:13 +0900

    Chapter 5 ex. 4

---
+++ 5.4_Alien_Colors_2.py
@@ -0,0 +1,11 @@
+alien_color = 'green'
+
+if alien_color == 'green':
+    print("You just earned 5 points for shooting the alien!")
+else:
+    print("You just earned 10 points!")
+
+if alien_color == 'blue':
+    print("You just earned 5 points for shooting the alien!")
+else:
+    print("You just earned 10 points!")

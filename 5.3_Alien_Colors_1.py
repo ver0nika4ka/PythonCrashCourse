594a461f1a5449abd3573665f70f32a7103cfc53
Veronica <veranika.aizu@gmail.com>
2020-03-20 18:05:22 +0900

    Chapter 5 ex. 3

---
+++ 5.3_Alien_Colors_1.py
@@ -0,0 +1,9 @@
+alien_color = 'green'
+# Write an if statement to test whether the alien’s color is green. 
+# If it is, print a message that the player just earned 5 points.
+if alien_color == 'green':
+    print("You earned 5 points.")
+
+alien_color = 'blue'
+if alien_color == 'green':
+    print("You earned 5 points.")

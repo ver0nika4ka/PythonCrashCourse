deb2ead5867e79e851827fbd704e790695468195
Veronica <veranika.aizu@gmail.com>
2020-03-20 19:54:51 +0900

    Chapter 5 ex. 7

---
+++ 5.7_Favorite_Fruit.py
@@ -0,0 +1,16 @@
+favorite_fruits = ['strawberry','pineapple','mango']
+if 'strawberry' in favorite_fruits:
+    fruit = 'strawberry'
+print(f"You really like {fruit}!")
+
+if 'pineapple' in favorite_fruits:
+    fruit = 'pineapple'
+print(f"You really like {fruit}!")
+
+if 'mango' in favorite_fruits:
+    fruit = 'mango'
+print(f"You really like {fruit}!")
+
+if 'apple' in favorite_fruits:
+    fruit = 'apple'
+print(f"You really like {fruit}!")

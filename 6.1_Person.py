1c099fa6def6649ec192f563bbbea00e812a636d
Veronica <veranika.aizu@gmail.com>
2020-03-25 12:41:49 +0900

    Chapter 6 ex. 1

---
+++ 6.1_Person.py
@@ -0,0 +1,9 @@
+user = {'first_name': 'alex',
+        'second_name': 'smith',
+        'age': 30,
+        'city': 'new york',
+        }
+print(f"User information:\n{user['first_name'].title()} "
+    f"{user['second_name'].title()}"
+    f"\nAge:{user['age']}"
+    f"\nCity: {user['city'].title()}")

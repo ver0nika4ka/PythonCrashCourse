9213d3b2af514232c84075c8c0d934289dd9f4ef
Veronica <veranika.aizu@gmail.com>
2020-03-26 14:53:45 +0900

    Chapter 6 ex. 4

---
+++ 6.4_Glossary_2.py
@@ -0,0 +1,7 @@
+glossary = {'list': 'an array of any object',
+            'len()': 'function to calculate length',
+            'tuples': 'list which you can not change',
+            'slicing': 'to determine how to slice the sequence',
+            'dictionary': 'similar to a list, but connect pieces as key-value pairs'}
+for key, value in glossary.items():
+    print(f"\n{key.title()} – {value}.")

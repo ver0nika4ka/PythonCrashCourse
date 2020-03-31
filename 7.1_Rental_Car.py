47d43908ffc95e03ffa3bf5f4fce0eaea7eb1634
Veronica <veranika.aizu@gmail.com>
2020-03-31 16:42:39 +0900

    Chapter 7 ex. 1

---
+++ 7.1_Rental_Car.py
@@ -0,0 +1,4 @@
+# Program asks the user what kind of rental car they would like.
+prompt = "What kind of car would you like?"
+car_name = input(prompt)
+print(f"Let me see if I can find you a {car_name.title()}.")

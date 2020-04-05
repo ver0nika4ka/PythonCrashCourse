76654e878c372a6590a49bbbfa9c311efa7318bb
Veronica <veranika.aizu@gmail.com>
2020-04-05 11:57:05 +0900

    Chapter 8 ex. 1

---
+++ 8.1_Message.py
@@ -0,0 +1,5 @@
+''' Function that prints one sentence telling what you are learning in this chapter. 
+Call the function, and make sure the message displays correctly.'''
+def display_message():
+    print("In chapter 8 you’ll learn about functions!")
+display_message()

1fca7eb7f330e360360ce1234feaa0d553565e4d
Veronica <veranika.aizu@gmail.com>
2020-04-09 10:56:13 +0900

    Chapter 8 ex. 6

---
+++ 8.6_City_Names.py
@@ -0,0 +1,6 @@
+def city_country(city, country):
+    print(f"{city.title()} in {country.title()}")
+
+city_country('tokyo', 'japan')
+city_country(city='minsk', country='belarus')
+city_country(country='france', city='paris')

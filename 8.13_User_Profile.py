5e8baca925ed3d89d9c390321ea44b54ef1340a8
Veronica <veranika.aizu@gmail.com>
2020-04-16 08:39:45 +0900

    Chapter 8 ex. 13

---
+++ 8.13_User_Profile.py
@@ -0,0 +1,10 @@
+def build_profile(first, last, **user_info):
+    """Build a dictionary containing everything we know about a user."""
+    user_info['first_name'] = first
+    user_info['last_name'] = last
+    return user_info
+user_profile = build_profile('veronica', 'mikhailova',
+                             location='japan',
+                             field='computer science',
+                             languages='python')
+print(user_profile)

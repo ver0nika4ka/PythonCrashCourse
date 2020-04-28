3353439ecb305327e4eea664eab135f4545e80dc
Veronica <veranika.aizu@gmail.com>
2020-04-28 10:30:10 +0900

    Chapter 10 ex. 13

---
+++ 10.13_Verify_User.py
@@ -0,0 +1,37 @@
+import json
+
+def get_stored_username():
+    """Get stored username if available."""
+    filename = 'username.json'
+    try:
+        with open(filename) as f:
+            username = json.load(f)
+    except FileNotFoundError:
+        return None
+    else:
+        return username
+
+def get_new_username():
+    """Prompt for a new username."""
+    username = input("What is your name? ")
+    filename = 'username.json'
+    with open(filename, 'w') as f:
+        json.dump(username, f)
+    return username
+
+def greet_user():
+    """Greet the user by name."""
+    username = get_stored_username()
+    if username:
+        answer = input(f"Are you {username}? Enter yes/no: ")
+        if answer.lower() == 'yes':
+            print(f"Welcome back, {username.title()}!")
+        else:
+            username = get_new_username()
+            print(f"We'll remember you when you come back, {username}!")
+    else:
+        username = get_new_username()
+        print(f"We'll remember you when you come back, {username}!")
+
+if __name__ == '__main__':
+    greet_user()

a6efc58f8d2692ae45cee1782f0aafd629069ed0
Veronica <veranika.aizu@gmail.com>
2020-10-11 12:48:38 +0900

    Project 3 Django Blog

---
+++ project_3_django/blog/blogs/urls.py
@@ -0,0 +1,22 @@
+"""Defines URL patterns for blogs.""" 
+
+from django.urls import path
+from . import views 
+
+app_name = 'blogs' 
+
+urlpatterns = [  
+    # Home page 
+    path('', views.index, name='index'),
+    # Page shows all the blogposts
+    path('blogposts/', views.blogposts, name='blogposts'),
+    # Detailed info about each blogpost
+    path('blogposts/<int:blogpost_id>/', views.blogpost, name='blogpost'),
+    # Page for adding a new blogpost
+    path('new_post/', views.new_post, name='new_post'),
+    # Edit blogpost
+    path('edit_post/<int:blogpost_id>/', views.edit_post, name='edit_post'),
+    
+
+
+]
---
+++ project_3_django/blog/blogs/views.py
@@ -0,0 +1,58 @@
+from django.shortcuts import render, redirect
+from .models import BlogPost
+from .forms import BlogPostForm
+
+# Create your views here.
+def index(request):
+    """Home page for Blog."""
+    return render(request, 'index.html')
+
+def blogposts(request):
+    """Show all blogposts on the page."""
+    blogposts = BlogPost.objects.order_by('date_added')
+    context = {'blogposts': blogposts}
+    return render(request, 'blogposts.html', context)
+
+def blogpost(request, blogpost_id):
+    """Show one post on the page."""
+    blogpost = BlogPost.objects.get(id=blogpost_id)
+    context = {'blogpost': blogpost}
+    return render(request, 'blogpost.html', context)
+
+def new_post(request):
+    """Add a new blogpost."""
+    if request.method != 'POST':
+        # if GET - create a blank form for new post.
+        form = BlogPostForm()
+    else:
+        # POST data submitted; process data.
+        form = BlogPostForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('blogs:blogposts')
+    
+    # Display a blank or invalid form.
+    context = {'form': form}
+    return render(request,'new_post.html', context)
+
+def edit_post(request, blogpost_id):
+    """Edit existing blogpost."""
+    blogpost = BlogPost.objects.get(id=blogpost_id)
+
+    if request.method != 'POST':
+        # If GET, pre-fill form with the current post.
+        form = BlogPostForm(instance=blogpost)
+    else:
+        # POST data submitted, process data.
+        form = BlogPostForm(instance=blogpost, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('blogs:blogpost', blogpost_id=blogpost.id)
+
+    context = {'blogpost': blogpost, 'form': form} 
+    return render(request,'edit_post.html', context)
+
+
+
+
+

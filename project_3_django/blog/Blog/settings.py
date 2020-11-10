4e0bbada70ea441d351392818518693c37c291d7
Veronica <veranika.aizu@gmail.com>
2020-11-10 14:24:35 +0900

    Set up user accounts for Blog

--- project_3_django/blog/Blog/settings.py
+++ project_3_django/blog/Blog/settings.py
@@ -31,7 +31,11 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # My apps
     'blogs',
+    'users',
+    
+    # Default django apps.
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- project_3_django/blog/Blog/urls.py
+++ project_3_django/blog/Blog/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('users/', users.urls),
     path('', include('blogs.urls')),
 ]

8379fa2a121a4f64116d1ba7aa2ca239bb93b48c
Veronica <veranika.aizu@gmail.com>
2020-11-10 21:31:37 +0900

    Move files to another directory

--- project_3_django/blog/blogs/views.py
+++ project_3_django/blog/blogs/views.py
@@ -5,19 +5,19 @@ from .forms import BlogPostForm
 # Create your views here.
 def index(request):
     """Home page for Blog."""
-    return render(request, 'index.html')
+    return render(request, 'blogs/index.html')
 
 def blogposts(request):
     """Show all blogposts on the page."""
     blogposts = BlogPost.objects.order_by('date_added')
     context = {'blogposts': blogposts}
-    return render(request, 'blogposts.html', context)
+    return render(request, 'blogs/blogposts.html', context)
 
 def blogpost(request, blogpost_id):
     """Show one post on the page."""
     blogpost = BlogPost.objects.get(id=blogpost_id)
     context = {'blogpost': blogpost}
-    return render(request, 'blogpost.html', context)
+    return render(request, 'blogs/blogpost.html', context)
 
 def new_post(request):
     """Add a new blogpost."""
@@ -33,7 +33,7 @@ def new_post(request):
     
     # Display a blank or invalid form.
     context = {'form': form}
-    return render(request,'new_post.html', context)
+    return render(request,'blogs/new_post.html', context)
 
 def edit_post(request, blogpost_id):
     """Edit existing blogpost."""
@@ -50,7 +50,7 @@ def edit_post(request, blogpost_id):
             return redirect('blogs:blogpost', blogpost_id=blogpost.id)
 
     context = {'blogpost': blogpost, 'form': form} 
-    return render(request,'edit_post.html', context)
+    return render(request,'blogs/edit_post.html', context)
 
 
 
